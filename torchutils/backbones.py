93d8971b4ef3dc26b82a96f96b41aa555e9df122
Zhi Lu <zhilu@std.uestc.edu.cn>
2021-04-03 22:19:48 +0800

    feat: add resnet with affine BN
    To fine-tune resnet in multiple GPU,
    we can repalce the BN with simple affine layer.
    
    Thus, I add a new group backbones
    that using simple affine layer.

--- torchutils/backbones.py
+++ torchutils/backbones.py
@@ -1,5 +1,6 @@
 from functools import partial, wraps
-
+import operator
+import torch
 import torch.nn as nn
 from torchvision import models
 from .overrides import set_module
@@ -96,5 +97,92 @@ def resnet101(pretrained=True, **kwargs):
 def resnet152(pretrained=True, **kwargs):
     num_features = 2048
     backbone = models.resnet152(pretrained, **kwargs)
+    backbone.fc = nn.Identity()
+    return backbone, num_features
+
+
+class AffineBatchNorm2d(nn.Module):
+    """BatchNorm2d without tracking."""
+
+    def __init__(self, num_features):
+        super().__init__()
+        self.register_buffer("running_mean", torch.zeros(num_features))
+        self.register_buffer("running_var", torch.ones(num_features))
+        self.register_buffer("num_batches_tracked", torch.ones(1))
+        self.weight = nn.Parameter(torch.ones(num_features))
+        self.bias = nn.Parameter(torch.zeros(num_features))
+        self.num_features = num_features
+        self.eps = 1e-5
+
+    def extra_repr(self):
+        return "{num_features}, eps={eps}".format(**self.__dict__)
+
+    def _t(self, w):
+        return w.view(self.num_features, 1, 1)
+
+    def forward(self, x):
+        h = (x - self._t(self.running_mean)) / torch.sqrt(self._t(self.running_var) + self.eps)
+        return self._t(self.weight) * h + self._t(self.bias)
+
+
+def _replace_bn(resnet: nn.Module):
+    state_dict = resnet.state_dict()
+    to_replace = []
+    for name, module in resnet.named_modules():
+        if "bn" in name:
+            to_replace.append((name, AffineBatchNorm2d(module.num_features)))
+    for name, value in to_replace:
+        name = name.split(".")
+        if len(name) == 1:
+            setattr(resnet, name[0], value)
+        else:
+            name1 = ".".join(name[:-1])
+            name2 = name[-1]
+            setattr(operator.attrgetter(name1)(resnet), name2, value)
+    resnet.load_state_dict(state_dict)
+    return resnet
+
+
+@register_backbone
+def resnet18_affine(pretrained=True, **kwargs):
+    num_features = 512
+    backbone = models.resnet18(pretrained, **kwargs)
+    backbone = _replace_bn(backbone)
+    backbone.fc = nn.Identity()
+    return backbone, num_features
+
+
+@register_backbone
+def resnet34_affine(pretrained=True, **kwargs):
+    num_features = 512
+    backbone = models.resnet34(pretrained, **kwargs)
+    backbone = _replace_bn(backbone)
+    backbone.fc = nn.Identity()
+    return backbone, num_features
+
+
+@register_backbone
+def resnet50_affine(pretrained=True, **kwargs):
+    num_features = 2048
+    backbone = models.resnet50(pretrained, **kwargs)
+    backbone = _replace_bn(backbone)
+    backbone.fc = nn.Identity()
+    return backbone, num_features
+
+
+@register_backbone
+def resnet101_affine(pretrained=True, **kwargs):
+    num_features = 2048
+    backbone = models.resnet101(pretrained, **kwargs)
+    backbone = _replace_bn(backbone)
+    backbone.fc = nn.Identity()
+    return backbone, num_features
+
+
+@register_backbone
+def resnet152_affine(pretrained=True, **kwargs):
+    num_features = 2048
+    backbone = models.resnet152(pretrained, **kwargs)
+    backbone = _replace_bn(backbone)
     backbone.fc = nn.Identity()
     return backbone, num_features
