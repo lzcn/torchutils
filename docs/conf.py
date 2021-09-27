f3438582919eab9a1a2deb8e8b9926f90b2a1bbe
Zhi Lu <zhilu@std.uestc.edu.cn>
2021-09-27 20:04:51 +0800

    doc(sphinx): change theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".AppleDouble"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -57,17 +57,14 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"  # "sphinxdoc"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_theme_options = {
-    "repository_url": "https://github.com/lzcn/torchutils",
-    "use_repository_button": True,
-}
+html_title = "torchutils"
 
 
 # -- Options for intersphinx extension ---------------------------------------
