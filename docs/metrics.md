99ffb30f4833c6316908463389f5100fb4085ba5
Zhi Lu <zhilu@std.uestc.edu.cn>
2021-11-30 20:39:22 +0800

    add class for metric

--- docs/metrics.md
+++ docs/metrics.md
@@ -1,7 +1,10 @@
-torchutils.metrics
-------------------
+## torchutils.metrics
 
+## Module API
+
+```{eval-rst}
 .. currentmodule:: torchutils.metrics
 
 .. automodule:: torchutils.metrics
     :members:
+```
--- torchutils/metrics.py
+++ torchutils/metrics.py
@@ -1,5 +1,7 @@
+import json
 import logging
-from typing import List, Tuple
+from collections import defaultdict
+from typing import Any, Dict, List, Tuple
 
 import numpy as np
 from sklearn.metrics import roc_auc_score
@@ -146,3 +148,153 @@ def margin_loss(pos, neg, margin=0.1):
     pos = np.maximum(1.0 - margin - np.array(pos), 0) ** 2
     neg = np.maximum(np.array(neg) - margin, 0) ** 2
     return (pos.sum() + neg.sum()) / (pos.size + neg.size)
+
+
+class Metric:
+    def __init__(self, input_transform=lambda x: x) -> None:
+        self._input_transform = input_transform
+        self._y_true = []
+        self._y_pred = []
+
+    def reset(self):
+        self._y_true = []
+        self._y_pred = []
+
+    def update(self, input: Any) -> None:
+        y_pred, y_true = self._input_transform(input)
+        self._y_pred.extend(y_pred)
+        self._y_true.extend(y_true)
+
+    def compute(self):
+        return self(self._y_pred, self._y_true)
+
+    def save(self, file):
+        results = self.compute()
+        data = {"y_pred": self._y_pred, "y_true": self._y_true}
+        data.update(**results)
+        with open(file, "w") as f:
+            json.dump(data, f)
+
+    def __call__(self, y_pred, y_true) -> Any:
+        raise NotImplementedError
+
+
+class ROC_AUC_Score(Metric):
+    def __call__(self, y_pred, y_true) -> Any:
+        return roc_auc_score(y_true, y_pred)
+
+
+class NDCG_Score(Metric):
+    def __call__(self, y_pred, y_true) -> Any:
+        return ndcg(y_true, y_pred).mean()
+
+
+class SoftMarginLoss(Metric):
+    r"""Metric for a two-class classification logistic loss between input :math:`x` and target :math:`y`.
+
+    The label of postivie class should be 1.
+
+    .. math::
+
+        \ell=\sum_{i} \frac{\log\left(1+\exp(-\mathbb{1}_{(y_i=1)}x_i + \mathbb{1}_{(y_i\neq1)}x_i))\right)}{n}
+
+
+    """
+
+    def __call__(self, y_pred, y_true) -> Any:
+        if len(y_pred) == 0:
+            raise RuntimeError("Metric must have at least one example before it can be computed.")
+        label_set = np.unique(y_true)
+        num_classes = len(label_set)
+        assert num_classes == 2, "SoftMarginLoss only supports binary classification"
+        assert 1 in label_set, "The label of positive class must be 1"
+        y = np.where(y_true == 1, 1, -1)
+        x = np.array(y_pred)
+        return np.log(1.0 + np.exp(-y * x)).sum() / x.size
+
+
+class BPRLoss(Metric):
+    r"""BPR metric for a two-class classification logistic loss between input :math:`x`
+    and target :math:`y`.
+
+    The label of postivie class should be :math:`1`. The :class:`BPRLoss` is similar to
+    :class:`SoftMarginLoss`. We first split the score :math:`x` into two parts
+    :math:`x^+\in\mathbb{R}^{n^+},x^-\in\mathbb{R}^{n^-}` according to the label
+    :math:`y`. And the loss is defined as:
+
+    .. math::
+
+        \ell=\sum_{i,j} \frac{\log\left(1+\exp(-(x^+_i - x_j^-))\right)}{n^+n^-}
+
+    If you want to compute the BPR loss of a list of tuples :math:`(x^+_i, x^-_i)`, use
+    the :class:`SoftMarginLoss` and update the results with :math:`(x^+_i - x^-_i, 1)`
+
+    """
+
+    def __call__(self, y_pred, y_true) -> Any:
+        label_set = np.unique(y_true)
+        num_classes = len(label_set)
+        assert num_classes == 2, "BPRLoss only supports binary classification"
+        assert 1 in label_set, "The label of positive class must be 1"
+        score = np.array(y_pred).flatten()
+        label = np.array(y_true).flatten()
+        pos_idx = np.where(label == 1)
+        neg_idx = np.where(label != 1)
+        pos_score = score[pos_idx]
+        neg_score = score[neg_idx]
+        if len(pos_score) == len(neg_score):
+            diff = pos_score - neg_score
+        else:
+            diff = pos_score.reshape(-1, 1) - neg_score.reshape(1, -1)
+            diff = diff.flatten()
+        return np.log(1.0 + np.exp(-diff)).sum() / diff.size
+
+
+class MetricList(Metric):
+    def __init__(self, metric_list, input_transform=lambda x: x) -> None:
+        super().__init__(input_transform=input_transform)
+        self.metric_list = metric_list
+
+    def __call__(self, y_pred, y_true) -> Any:
+        results = []
+        for metric in self.metric_list:
+            results.append(metric(y_pred, y_true))
+        return results
+
+
+class MetricDcit(Metric):
+    def __init__(self, metric_dict, input_transform=lambda x: x) -> None:
+        super().__init__(input_transform=input_transform)
+        self.metric_dict = metric_dict
+
+    def __call__(self, y_pred, y_true) -> Any:
+        results = dict()
+        for key, metric in self.metric_dict.items():
+            results[key] = metric(y_pred, y_true)
+        return results
+
+
+class UserMetricDict(Metric):
+    def __init__(self, metric_dict, input_transform=lambda x: x):
+        self._y_pred = defaultdict(list)
+        self._y_true = defaultdict(list)
+        self._output_transform = input_transform
+        self.metric_dict = metric_dict
+
+    def reset(self):
+        self._y_pred = defaultdict(list)
+        self._y_true = defaultdict(list)
+
+    def update(self, *output: List[list]):
+        y_pred, y_true, uidx = self._output_transform(*output)
+        for x, y, u in zip(y_pred, y_true, uidx):
+            self._y_pred[u].append(x)
+            self._y_true[u].append(y)
+
+    def __call__(self, y_pred, y_true) -> Dict:
+        metrics = defaultdict(list)
+        uidx = y_pred.keys()
+        for u in uidx:
+            for key, metric in self.metric_dict.items():
+                metrics[key].append(metric(np.array(y_pred[u]), np.array(y_true[u])))
+        return {k: np.sum(v) / len(v) for k, v in metrics.items()}
